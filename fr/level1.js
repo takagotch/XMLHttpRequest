a6202f3653452aa0e072d82935e88bc19e61fc77
takagotch <dyaccb@gmail.com>
2018-02-13 03:16:39 +0900

    var win = document.getElementById().contentWindow; win.postMessage();

--- fr/level1.js
+++ fr/level1.js
@@ -1,11 +1,28 @@
 //req.method
+open(/*String*/ method, /*String*/ url)
+open(/*String*/ method, /*String*/ url, /*String*/ async, /*boolean*/ user)
+open(/*String*/ method, /*String*/ url, /*String*/ async, /*boolean*/ user)
+open(/*String*/ method, /*String*/ url, /*String*/ async, /*boolean*/ user, /*String*/ password)
+setRequestHeader(/*String*/ header, /*String*/ value)
+send()
+send(/*Document*/ data)
+send(/*String*/ data)
+abort()
 
 //res.property
+status
+statusText
+responseText
+responseXML
 
 //res.method
+getResponseHeader(/*String*/ header)
+getAllResponseHeaders()
 
 //event.method
+onreadystatechange
 
 //state [0-4]
+readyState
 
 
--- fr/post.js
+++ fr/post.js
@@ -1,3 +1,16 @@
 var xhr = getXMLHttpRequest();
 
+xhr.onreadystatechange = function(){
+  if(xhr.readyState === 4){
+    if(xhr.status === 200){
+      assert('SENDTEST', 'ECHOTEST', xhr.responseText);
+    }else{
+      assert('Error', 200, xhr.status);
+    }
+  }
+};
+
+xhr.open("POST", "/echo/html/");
+xhr.setRequestHeader("Content-Type", "text/plain; charset=UTF-8");
+xhr.send("html=ECHOTEST");
 
